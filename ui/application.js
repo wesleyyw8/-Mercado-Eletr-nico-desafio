b7884d51ce1bc62f39558a349d954627067e9dd4
Wesley Rebelo <wesleyyw8@gmail.com>
2016-02-21 13:10:17 -0300

    sidebar ng-repeat

--- ui/application.js
+++ ui/application.js
@@ -12,7 +12,43 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider,$loca
 }]);
 
 app.controller('IndexController', ['$scope','$http','$routeParams', function($scope,$http, routeParams){
-	
+	populateSideBar();
+	function populateSideBar(){
+		/*fa fa-home*/
+		$scope.sideBarItems = [];
+		$scope.sideBarItems.push({
+			icon: 'fa fa-home',
+			text: 'Home'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-align-left',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-usd',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-shopping-cart',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-globe',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-globe',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-pencil',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-question-circle',
+			text: 'Lorem Ipsum'
+		});
+	}
 }]); 
 
 app.controller('PrincipalController', ['$scope','$http','$routeParams','$datepicker','$timeout', function($scope,$http, routeParams, $datepicker, $timeout){
--- ui/controllers/index.js
+++ ui/controllers/index.js
@@ -1,3 +1,39 @@
 app.controller('IndexController', ['$scope','$http','$routeParams', function($scope,$http, routeParams){
-	
+	populateSideBar();
+	function populateSideBar(){
+		/*fa fa-home*/
+		$scope.sideBarItems = [];
+		$scope.sideBarItems.push({
+			icon: 'fa fa-home',
+			text: 'Home'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-align-left',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-usd',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-shopping-cart',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-globe',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-globe',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-pencil',
+			text: 'Lorem Ipsum'
+		});
+		$scope.sideBarItems.push({
+			icon: 'fa fa-question-circle',
+			text: 'Lorem Ipsum'
+		});
+	}
 }]); 
