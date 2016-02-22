6b0ad1684f634f7223a4fb810a23c6cad879331f
Wesley Rebelo <wesleyyw8@gmail.com>
2016-02-21 21:30:15 -0300

    project done

--- ui/application.js
+++ ui/application.js
@@ -1,4 +1,4 @@
-var app = angular.module('desafioMercadoEletronicoApp',['ngRoute']);
+var app = angular.module('desafioMercadoEletronicoApp',['ngRoute','toaster', 'ngAnimate']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
 	$routeProvider.
@@ -55,28 +55,50 @@ app.controller('IndexController', ['$scope','$http','$routeParams', function($sc
 	}
 }]); 
 
-app.controller('LoginController', ['$scope','$http','$routeParams', function($scope,$http, routeParams){
+app.controller('LoginController',
+['$scope','$http','$routeParams','toaster','$location', function($scope,$http,
+routeParams,toaster,location){
 
 	var emailRegex = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 	//at least 1 letter, at least 1 number, 3 to 13 characters and 1 special char
 	var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{3,13}$/;
 
-	$scope.savePassword = function(){
-		checkValidFields();
+	$scope.user ={
+		email: "",
+		newPassword: "",
+		confirmNewPassword: "",
+		password: ""
 	};
+
+	$scope.savePassword = function(){
+		if (checkValidFields())
+			location.path("/principal");
+	};
+	function toasterMessage(msg){
+		toaster.pop({type: 'error', "title": msg});
+	}
 	function checkValidFields(){
-		if (!emailRegex.test($scope.user.email))
-			alert('O campo email deve conter um email valido');
-		
-		if (!passwordRegex.test($scope.user.newPassword))
-			console.log("O campo nova senha deve conter pelo menos:"+
+		if (!emailRegex.test($scope.user.email)){
+			toasterMessage('O campo email deve conter um email valido');
+			return false;
+		}
+		else if ($scope.user.password.length < 1){
+			toasterMessage("Preencha o campo senha");
+			return false;
+		}
+		else if (!passwordRegex.test($scope.user.newPassword)){
+			toasterMessage("O campo nova senha deve conter pelo menos:"+
 						"Uma letra;"+
 						"Um número;"+
 						"Um caractere especial;"+
 						"Mais que 3 caracteres e menos que 13 caracteres");
-
-		if ($scope.user.newPassword != $scope.user.confirmNewPassword)
-			console.log('O campo confirme a nova senha deve conter a mesma senha que o campo nova senha');
+			return false;
+		}
+		else if ($scope.user.newPassword != $scope.user.confirmNewPassword){
+			toasterMessage('O campo confirme a nova senha deve conter a mesma senha que o campo nova senha');
+			return false;
+		}
+		return true;
 	};
 }]);
 app.controller('PrincipalController', ['$scope','$http','$routeParams','$timeout', function($scope,$http, routeParams, $timeout){
--- ui/controllers/config.js
+++ ui/controllers/config.js
@@ -1,4 +1,4 @@
-var app = angular.module('desafioMercadoEletronicoApp',['ngRoute']);
+var app = angular.module('desafioMercadoEletronicoApp',['ngRoute','toaster', 'ngAnimate']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
 	$routeProvider.
